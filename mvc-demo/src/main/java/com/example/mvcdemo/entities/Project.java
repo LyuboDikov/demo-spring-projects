83182adaf91a022cac66eaba5736a11d7b23f431
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-19 18:28:11 +0300

    Importing files from resources

---
+++ mvc-demo/src/main/java/com/example/mvcdemo/entities/Project.java
@@ -0,0 +1,72 @@
+package com.example.mvcdemo.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "projects")
+public class Project extends BaseEntity{
+
+    private String name;
+    private String description;
+    private boolean isFinished;
+    private BigDecimal payment;
+    private Date startDate;
+    private Company company;
+
+
+    @Column(nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "is_finished")
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(boolean finished) {
+        isFinished = finished;
+    }
+
+    public BigDecimal getPayment() {
+        return payment;
+    }
+
+    public void setPayment(BigDecimal payment) {
+        this.payment = payment;
+    }
+
+    @Column(columnDefinition = "DATE")
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    @ManyToOne
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
