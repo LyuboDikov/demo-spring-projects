bb1267fc6de4e295265a72d898386bea2ce7ad00
LyuboDikov <lyubo.dikov@icloud.com>
2023-07-19 20:24:06 +0300

    Implemented UserLoginDto and the business logic behind it.

--- mvc-demo/src/main/java/com/example/mvcdemo/services/UserService.java
+++ mvc-demo/src/main/java/com/example/mvcdemo/services/UserService.java
@@ -1,9 +1,12 @@
 package com.example.mvcdemo.services;
 
+import com.example.mvcdemo.dtos.UserLoginDto;
 import com.example.mvcdemo.dtos.UserRegisterDto;
 
 public interface UserService {
 
     boolean register(UserRegisterDto user);
+
+    Long validateUserLoginDetails(UserLoginDto user);
 
 }
